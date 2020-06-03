586340665d108dc381669ef8dbbe0d33b9594887
kobayashiukyo <u.k.knua0419@gmail.com>
2020-06-03 16:51:57 +0900

    finish

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+	# before_action :baria_user, only: [:update]
+  before_action :authenticate_user!
+  def show
+  	@user = User.find(params[:id])
+  	@books = @user.books
+  	@book_new = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+  end
+
+  def index
+  	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
+  	@book_new = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+    @user = User.find(current_user.id)
+    @users =User.all
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+    if @user != current_user
+          redirect_to current_user
+    end
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		redirect_to user_path(@user.id), notice: "successfully updated user!"
+  	else
+  		render "edit"
+  	end
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  #url直接防止　メソッドを自己定義してbefore_actionで発動。
+   def baria_user
+  	unless params[:id].to_i == current_user.id
+  		redirect_to user_path(current_user)
+  	end
+   end
+
+end
